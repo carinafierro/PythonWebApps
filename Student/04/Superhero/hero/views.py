1e5ba67fa04966b716f5ce591f992e04bb61a355
Carina Fierro <carinafierro@yahoo.com>
2024-10-21 23:13:58 -0600

    project 3

--- Student/04/Superhero/hero/views.py
+++ Student/04/Superhero/hero/views.py
@@ -1,5 +1,8 @@
+from typing import Any
 from django.views.generic import TemplateView
 
 class SuperheroView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        return {'hero': "static\images\captain_america.jpg"}
