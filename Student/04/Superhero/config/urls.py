983bb6048a50f0bf834f46bcff25aa511adbae38
Carina Fierro <carinafierro@yahoo.com>
2024-10-31 19:56:08 -0600

    project 4

--- Student/04/Superhero/config/urls.py
+++ Student/04/Superhero/config/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.admin import site
 from hero.views import SuperheroListView, SuperheroView
 
 urlpatterns = [
     path('<str:name>', SuperheroView.as_view()),
     path('', SuperheroListView.as_view()),
+    path(r'admin/', site.urls),
 ]
--- Student/04/Superhero/hero/models.py
+++ Student/04/Superhero/hero/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class Superhero(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length=200)  # URL or path for the image
+
+    def __str__(self):
+        return self.name
