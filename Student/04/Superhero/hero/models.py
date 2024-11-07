dc107452424b674fc6374ff254ca0fc64ffebe5e
Carina Fierro <carinafierro@yahoo.com>
2024-11-06 18:18:52 -0700

    project 4

--- Student/04/Superhero/hero/models.py
+++ Student/04/Superhero/hero/models.py
@@ -5,3 +5,6 @@ class Superhero(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = models.CharField(max_length=200)
+
+def __str__(self):
+    return f'{self.pk}. {self.name} - {self.description}'
