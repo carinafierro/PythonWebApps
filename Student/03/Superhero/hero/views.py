c30c0ea4a9ca2bdbf2b8627d1aa863d2629622c6
Carina Fierro <carinafierro@yahoo.com>
2024-11-14 22:26:34 -0700

    project 3

--- Student/03/Superhero/hero/views.py
+++ Student/03/Superhero/hero/views.py
@@ -2,39 +2,44 @@ from pathlib import Path
 from django.views.generic import TemplateView 
 
 data = {
-    'batman': {
-        'name': 'Batman',
+    'hero': {
+        'title': 'Batman',
+        'id': 'Bruce Wayne',
         'image': '/static/images/batman.jpg',
         'strengths': 'smart, knows martial arts ',
         'weaknesses': 'no superpowers, injured easily, trust issues',
     },
-    'captain america': {
+    'hero': {
         'name': 'Captain America',
+        'id': 'Steve Rogers',
         'image': '/static/images/captain_america.jpg',
         'strengths': 'speed, endurance, smart',
         'weaknesses': 'limitations to injuries, isolated',
     },
-    'superman': {
+    'hero': {
         'name': 'Superman',
+        'id': 'Clark Kent',
         'image': '/static/images/superman.jpg',
         'strengths': 'super fast, heat vision, can fly',
         'weaknesses': 'kryptonite, isolated, overexertion',
     },
-    'wolverine': {
+    'hero': {
         'name': 'Wolverine',
+        'id': 'Logan',
         'image': '/static/images/wolverine.jpg',
         'strengths': 'good instincts, martial arts, good senses',
         'weaknesses': 'rage, adamantium, isolated',
     },
-    'wonder woman': {
-        'name': 'Womder Woman',
+    'hero': {
+        'name': 'Wonder Woman',
+        'id': 'Diana Prince',
         'image': '/static/images/wonder_woman.jpg',
         'strengths': 'strategic thinking, fast healing, combat skills',
         'weaknesses': 'over protetive, vulnerable to magic, isolated',
     },
 }
 class SuperheroView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'hero.html'
 
     def get_context_dat(self, **kwargs):
         return {'hero': data}
@@ -45,8 +50,9 @@ class SuperheroView(TemplateView):
         return {'hero': image}
     
     
+    
 class SuperheroListView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'heroes.html'
 
     def get_context_data(self, **kwargs):
         h = Path('static/images').iterdir()
