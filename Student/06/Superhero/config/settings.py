b64360f99834eefec70cf0ba23e3365f21754d4e
Carina Fierro <carinafierro@yahoo.com>
2024-11-18 16:03:32 -0700

    project 6

--- Student/06/Superhero/config/settings.py
+++ Student/06/Superhero/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h5y58o)^wp4*x$8)dzli)ifx*0vt=k1b$6%4qgd-o6bu!=mjjo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
