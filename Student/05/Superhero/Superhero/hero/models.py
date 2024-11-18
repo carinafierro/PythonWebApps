e555dbdf788d6748cfa137ee444818b2fa5da349
Carina Fierro <carinafierro@yahoo.com>
2024-11-18 15:33:38 -0700

    project 6

--- Student/05/Superhero/Superhero/hero/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Note(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    body = models.TextField()
