492ee545db043150b180ea4a63d25c9489614fdc
Carina Fierro <carinafierro@yahoo.com>
2024-11-18 16:44:08 -0700

    project 6

--- Student/06/Superhero/hero/views.py
+++ Student/06/Superhero/hero/views.py
@@ -7,3 +7,17 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class UserHomeView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        if self.request.user.is_anonymous:
+            return '/article/'
+        return f'/author/{get_me(self.request.user).pk}'
+
+def get_me(user):
+    return Author.objects.get_or_create(user=user)[0]
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class SecretView(LoginRequiredMixin, TemplateView):
+    template_name = "page.html"
