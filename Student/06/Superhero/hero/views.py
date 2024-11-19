8f13fa408e2b53bb32272f6240e1fc148ad6246f
Carina Fierro <carinafierro@yahoo.com>
2024-11-18 22:18:45 -0700

    project 7

--- Student/06/Superhero/hero/views.py
+++ Student/06/Superhero/hero/views.py
@@ -1,22 +1,3 @@
-from django.views.generic import TemplateView
-from django.views.generic import RedirectView
-from django.contrib.auth.forms import UserCreationForm
-from .models import Superhero
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
-
-class UserHomeView(RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        if self.request.user.is_anonymous:
-            return '/article/'
-        return f'/author/{get_me(self.request.user).pk}'
-
-def get_me(user):
-    return Author.objects.get_or_create(user=user)[0]
-
-class SecretView(LoginRequiredMixin, TemplateView):
-    template_name = "page.html"
+# Create your views here.
--- Student/07/Superhero/hero/models.py
+++ Student/07/Superhero/hero/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
-# Create your models here.
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=200)
+    identity = models.CharField(max_length=200)
+    description = models.TextField(default="None")
+    image = models.CharField(max_length=200, default="None")
+    strengths = models.CharField(max_length=200, default="None")
+    weaknesses = models.CharField(max_length=200, default="None")
+
+    def __str__(self):
+        return self.identity
+
+    def get_absolute_url(self):
+        return reverse_lazy('hero_list')
+
+class Author(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+    bio = models.TextField()
