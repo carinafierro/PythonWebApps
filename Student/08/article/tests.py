50a7b4b5541c20395633cd8069c864e994d0684f
Carina Fierro <carinafierro@yahoo.com>
2024-12-02 22:47:02 -0700

    project 8

--- Student/08/article/tests.py
+++ Student/08/article/tests.py
@@ -1,8 +1,13 @@
-from django.test import SimpleTestCase
-
-
+from django.test import SimpleTestCase, TestCase
+from .models import Article
 
 class ArticleAppTest(SimpleTestCase):
 
     def test_django(self):
         self.assertTrue(True)
+
+class ArticleDataTest(TestCase):
+    
+    def test_article(self):
+        Article.objects.create(title='title')
+        self.assertEqual(len(Article.objects.all()), 1)
