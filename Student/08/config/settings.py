df99d5b5044e42fce39f5c2104d5889ac3c53563
Carina Fierro <carinafierro@yahoo.com>
2024-12-04 00:24:32 -0700

    k

--- Student/08/config/settings.py
+++ Student/08/config/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static_assets/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Default primary key field type
