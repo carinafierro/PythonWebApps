8e62d0c0f0a37bfed35de2dd21f6673c667f7fa5
Carina Fierro <carinafierro@yahoo.com>
2024-12-05 16:21:34 -0700

    j

--- Student/10/superhero/hero/models.py
+++ Student/10/superhero/hero/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Superhero(models.Model):
+    name = models.CharField(max_length=200)
+    identity = models.CharField(max_length=200)
+    description = models.TextField(default="None")
+    image = models.CharField(max_length=200, default="None")
+    strengths = models.CharField(max_length=200, default="None")
+    weaknesses = models.CharField(max_length=200, default="None")
