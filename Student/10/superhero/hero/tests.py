95f4068a9c7b60fba55eb5be5fb1d19e3f61c6db
Carina Fierro <carinafierro@yahoo.com>
2024-12-05 20:30:57 -0700

    project 10

--- Student/10/superhero/hero/tests.py
+++ Student/10/superhero/hero/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
 from hero.models import Investigator, Superhero, Article
 
-class Test(TestCase):
+class InvestigatorTest(TestCase):
     def investigatortest(self):
-        investigator = Investigator.objects.create(name="")
+        investigator = Investigator.objects.create(name="Natasha")
+        self.assertEqual(investigator.name, "Natasha")
+
+class ArticleTest(TestCase):
+    def articletest(self):
+        article = Article.objects.create(title="Heroes")
+        self.assertEqual(article.title, "Heroes")
+
+class SuperheroTest(TestCase):
+    def superherotest(self):
+        superhero = Superhero.objects.create(name="batman")
+        self.assertEqual(superhero.identity, "Bruce Wayne")
