996bd27fcb83307bf0b276c101c1ff4f220f3a3f
Carina Fierro <carinafierro@yahoo.com>
2024-12-06 00:01:20 -0700

    k

--- Student/10/config/urls.py
+++ Student/10/config/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('doc/<str:doc>', DocumentView.as_view()),
 ]
--- Student/10/hero/views.py
+++ Student/10/hero/views.py
@@ -9,3 +9,18 @@ class CardView(TemplateView):
         title = "Gettysburg"
         body = 'Four score and seven years ago ...'
         return dict(title=title, body=body, color='bg-primary', width='col')
+    
+class CardsView(TemplateView):
+    template_name = 'cards.html'
+
+    def get_context_data(self, **kwargs):
+        return dict(cards=cards_data())
+    
+class DocumentView(TemplateView):
+    template_name = 'document.html'
+
+    def get_context_data(self, **kwargs):
+        document = self.kwargs.get('doc')
+        path = Path('Documents')/document
+        markdown_text = markdown(path.read_text())
+        return dict(text=markdown_text)
