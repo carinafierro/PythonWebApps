497df0fccdaa4bc56817730b5412e33d238646a5
Carina Fierro <carinafierro@yahoo.com>
2024-12-05 23:48:23 -0700

    11

--- Student/10/hero/views.py
+++ Student/10/hero/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class CardView(TemplateView):
+    template_name = 'card.html'
+
+    def get_context_data(self, **kwargs):
+        title = "Gettysburg"
+        body = 'Four score and seven years ago ...'
+        return dict(title=title, body=body, color='bg-primary', width='col')
